684533570d2a778b8d45157a66dd7fa5474a634e
Ahmad Gneady <support@bigprof.com>
2021-01-19 20:49:51 +0200

    As generated by AppGini 5.93

--- app/admin/incHeader.php
+++ app/admin/incHeader.php
@@ -14,7 +14,7 @@
 		<link id="browser_favicon" rel="shortcut icon" href="<?php echo PREPEND_PATH; ?>resources/table_icons/administrator.png">
 
 		<link rel="stylesheet" href="<?php echo PREPEND_PATH; ?>resources/initializr/css/cosmo.css">
-		<link rel="stylesheet" href="<?php echo PREPEND_PATH; ?>dynamic.css.php">
+		<link rel="stylesheet" href="<?php echo PREPEND_PATH; ?>dynamic.css">
 
 		<!--[if lt IE 9]>
 			<script src="<?php echo PREPEND_PATH; ?>resources/initializr/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
@@ -40,7 +40,7 @@
 			}
 
 			function jsShowWait() {
-				return window.confirm("<?php echo $Translation['sending mails']; ?>");
+				return window.confirm('<?php echo addslashes($Translation['sending mails']); ?>');
 			}
 
 			function jsValidateAdminSettings() {
@@ -69,30 +69,28 @@
 					var mm=document.getElementById('moveMembers').checked;
 				}
 
-				//confirm('sg='+sg+'\n'+'sm='+sm+'\n'+'dg='+dg+'\n'+'dm='+dm+'\n'+'mm='+mm+'\n'+'dmm='+dmm+'\n');
-
 				if(dmm && !dm) {
-					modal_window({ message: '<div>'+"<?php echo $Translation['complete step 4']; ?>"+'</div>', title: "<?php echo $Translation['info']; ?>", close: function() { /* */ jQuery('#destinationMemberID').focus(); } });
+					modal_window({ message: '<div>'+"<?php echo $Translation['complete step 4']; ?>"+'</div>', title: "<?php echo $Translation['info']; ?>", close: function() { jQuery('#destinationMemberID').focus(); } });
 					return false;
 				}
 
 				if(mm && sm!='-1') {
 
-					confirmMessage = "<?php echo $Translation['sure move member']; ?>";
+					confirmMessage = '<?php echo addslashes($Translation['sure move member']); ?>';
 					confirmMessage = confirmMessage.replace(/<MEMBER>/, sm).replace(/<OLDGROUP>/, sg).replace(/<NEWGROUP>/, dg);
 					return window.confirm(confirmMessage);
 
 				}
 				if((dmm || dm) && sm!='-1') {
 
-					confirmMessage = "<?php echo $Translation['sure move data of member']; ?>";
+					confirmMessage = '<?php echo addslashes($Translation['sure move data of member']); ?>';
 					confirmMessage = confirmMessage.replace(/<OLDMEMBER>/, sm).replace(/<OLDGROUP>/, sg).replace(/<NEWMEMBER>/, dm).replace(/<NEWGROUP>/, dg);                 
 					return window.confirm(confirmMessage);
 				}
 
 				if(mm) {
 
-					confirmMessage = "<?php echo $Translation['sure move all members']; ?>";
+					confirmMessage = '<?php echo addslashes($Translation['sure move all members']); ?>';
 					confirmMessage = confirmMessage.replace(/<OLDGROUP>/, sg).replace(/<NEWGROUP>/, dg);
 					return window.confirm(confirmMessage);
 				}
@@ -100,7 +98,7 @@
 				if(dmm) {
 
 
-					confirmMessage = "<?php echo $Translation['sure move data of all members']; ?>";
+					confirmMessage = '<?php echo addslashes($Translation['sure move data of all members']); ?>';
 					confirmMessage = confirmMessage.replace(/<OLDGROUP>/, sg).replace(/<MEMBER>/, dm).replace(/<NEWGROUP>/, dg);
 					return window.confirm(confirmMessage);
 				}
@@ -202,7 +200,6 @@
 						<ul class="dropdown-menu">
 							<li><a href="pageSettings.php"><i class="glyphicon menu-item-icon text-info glyphicon-cog"></i> <?php echo $Translation['admin settings']; ?></a></li>
 							<li class="divider"></li>
-							<li><a href="pageRebuildThumbnails.php"><i class="glyphicon menu-item-icon text-info glyphicon-picture"></i> <?php echo  $Translation['rebuild thumbnails']; ?></a></li>
 							<li><a href="pageRebuildFields.php"><i class="glyphicon menu-item-icon text-info glyphicon-refresh"></i> <?php echo  $Translation['view or rebuild fields']; ?></a></li>
 							<li><a href="pageUploadCSV.php"><i class="glyphicon menu-item-icon text-info glyphicon-upload"></i> <?php echo $Translation['import CSV']; ?></a></li>
 							<li><a href="pageTransferOwnership.php"><i class="glyphicon menu-item-icon text-info glyphicon-random"></i> <?php echo $Translation['batch transfer']; ?></a></li>
@@ -223,7 +220,7 @@
 									<?php
 										$plugin_icon = '';
 										if($plugin['glyphicon']) $plugin_icon = "<i class=\"glyphicon glyphicon-{$plugin['glyphicon']}\"></i> ";
-										if($plugin['icon']) $plugin_icon = "<img src=\"{$plugin['admin_path']}/{$plugin['icon']}\"> ";
+										if($plugin['icon']) $plugin_icon = "<img class=\"rspacer-md\" src=\"{$plugin['admin_path']}/{$plugin['icon']}\"> ";
 									?>
 									<li><a target="_blank" href="<?php echo $plugin['admin_path']; ?>"><?php echo $plugin_icon . $plugin['title']; ?></a></li>
 								<?php } ?>
