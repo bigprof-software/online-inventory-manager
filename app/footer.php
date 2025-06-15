cc84bdb1a291d2a63932b083472f04c1d1796ad9
Ahmed Genedy <github@bigprof.com>
2025-06-15 15:50:10 +0000

    Add footer notice for AppGini version 25.13

--- app/footer.php
+++ app/footer.php
@@ -4,6 +4,16 @@
 				<div style="height: 70px;" class="hidden-print"></div>
 			<?php } ?>
 
+			<?php if(!Request::val('Embedded')) { ?>
+				<!-- AppGini powered by notice -->
+				<div style="height: 60px;" class="hidden-print"></div>
+				<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation" style="z-index: 800;">
+					<p class="navbar-text"><small>
+						Powered by <a class="navbar-link" href="https://bigprof.com/appgini/" target="_blank">BigProf AppGini 25.13</a>
+					</small></p>
+				</nav>
+			<?php } ?>
+
 		</div> <!-- /div class="container" -->
 		<?php if(!defined('APPGINI_SETUP') && is_file(__DIR__ . '/hooks/footer-extras.php')) { include(__DIR__ . '/hooks/footer-extras.php'); } ?>
 	</body>
